934776ff35d2b44869002cd4c53f49cbcd73ed77
Xa9aX ツ <mishradiganta91@gmail.com>
2024-02-28 16:58:51 -0500

    update

--- cifar10.sh
+++ cifar10.sh
@@ -8,6 +8,6 @@ for class_name in "${class_names[@]}"; do
     output_dir="/home/mila/d/diganta.misra/scratch/sd_xl_images/$dataset/$class_name"
     mkdir -p "$output_dir"
 
-    sbatch /home/mila/d/diganta.misra/projects/mila.sh $class_name $output_dir
+    sbatch /home/mila/d/diganta.misra/projects/Gen_data/mila.sh $class_name $output_dir
 
 done
--- mila.sh
+++ mila.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH -c 8
-#SBATCH --job-name=LP_ViT_C10
+#SBATCH --job-name=gen_sdxl
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --reservation=ubuntu1804
 #SBATCH --signal=SIGUSR1@90 # 90 seconds before time limit
-#SBATCH --output=$SCRATCH/lpvit-%j.out
-#SBATCH --error=$SCRATCH/lpvit-%j.err
+#SBATCH --output=$SCRATCH/sdxl-%j.out
+#SBATCH --error=$SCRATCH/sdxl-%j.err
 
-pyfile=/home/mila/d/diganta.misra/projects/generate.py
+pyfile=/home/mila/d/diganta.misra/projects/Gen_data/generate.py
 
 module load anaconda/3
 
 conda activate /home/mila/d/diganta.misra/.conda/envs/sparse
 
-wandb login bd67cef57b7227730fe3edf96e11d954558a9d0d
-
 ulimit -Sn $(ulimit -Hn)
-
-wandb online
-
-WANDB_CACHE_DIR=$SCRATCH
 
 python3 $pyfile $1 $2
