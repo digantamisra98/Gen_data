684a8291ff33bf7b3f721b08ba04631ce4806138
Xa9aX ツ <mishradiganta91@gmail.com>
2024-02-28 21:03:59 -0500

    update

--- mila.sh
+++ mila.sh
@@ -4,7 +4,6 @@
 #SBATCH --job-name=gen_sdxl
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:rtx8000:1
-#SBATCH --reservation=ubuntu1804
 #SBATCH --signal=SIGUSR1@90 # 90 seconds before time limit
 #SBATCH --output=$SCRATCH/sdxl-%j.out
 #SBATCH --error=$SCRATCH/sdxl-%j.err
